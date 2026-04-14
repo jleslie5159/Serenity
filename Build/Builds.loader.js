765a136347260b18c8777479a7bc25fab16702b4
jleslie5159 <jleslie5159@gmail.com>
2026-04-14 13:50:33 -0700

    commit

--- Build/Builds.loader.js
+++ Build/Builds.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 79243090,
+    wasmFileSize: 79245089,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
